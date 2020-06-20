427d74323ade6b06ff641be748d90da6ac6f790d
magrini <magrinibruna@usp.br>
2020-06-20 09:26:01 -0300

    Iniciado Desafio 6

--- Desafio 6/javascript/index.js
+++ Desafio 6/javascript/index.js
@@ -4,7 +4,7 @@ const jokempo = require('./jokempo');
 const randomPhrases = require('./random-phrases');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR ACCESS TOKEN HERE';
+const token = require('./token');
 
 if (token === 'YOUR ACCESS TOKEN HERE') {
 	console.log('You forgot to replate the access token!!! Properly read the README before continuing >:(');
@@ -14,9 +14,12 @@ if (token === 'YOUR ACCESS TOKEN HERE') {
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', async (msg) => {
+bot.onText(/\/help/, async (msg) => {
+	bot.sendMessage(msg.chat.id, "Comandos:\n/jokempo - Inicia o jogo Pedra, Papel e Tesoura contra o bot.\n\nCódigo fonte: https://github.com/magrinibruna/desafios-quarentena/tree/master/Desafio 6");
+})
+
+// Listen for any kind of message. There are different kinds of messages.
+bot.onText(/\/start/, async (msg) => {
 	const chatMessage = msg.text.trim().toLowerCase();
 	const chatId = msg.chat.id;
 	if (chatMessage.startsWith('ola') || chatMessage.startsWith('oi')) {
